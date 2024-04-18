412866c0ee1a621fa0443efa0976297ebe56a9b9
Guillermo Ojeda <gro1413@hotmail.com>
2024-04-18 07:17:29 -0300

    fix chart style

--- src/components/chart/AreaChart.tsx
+++ src/components/chart/AreaChart.tsx
@@ -10,7 +10,7 @@ const AreaChartComponent = () => {
                 <defs>
                     <linearGradient id="colorGradient" x1="0" y1="0" x2="0" y2="1">
                         <stop offset="5%" stopColor="#0062BC" stopOpacity={1.5}/>
-                        <stop offset="95%" stopColor="#0062BC" stopOpacity={0.2}/>
+                        <stop offset="95%" stopColor="#00294F" stopOpacity={1.0}/>
                     </linearGradient>
                 </defs>
                 <XAxis dataKey='name' tick={{ fill: '#FFFFFF' }} axisLine={false} padding={{left: 20}} />
