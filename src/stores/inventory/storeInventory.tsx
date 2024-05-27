010f85aa775932036d06b8762ad8abe7c7cbdc4d
AlanAbelPereyra <alanabelpereyra322@gmail.com>
2024-05-27 04:21:12 -0300

    shipping and tracking

--- src/stores/inventory/storeInventory.tsx
+++ src/stores/inventory/storeInventory.tsx
@@ -7,5 +7,6 @@ export const storeInventory = (set: any) => ({
   setDataPerfilInventory: (value: any) => set({ DataPerfilInventory: value }),
   setInventory: (value: any) => set({ Inventory: value }),
   setDataInventorySearch: (value: any) => set({ DataInventorySearch: value }),
+
   // setAuthenticated: (value) => set({ Authenticated: value }),
 });
